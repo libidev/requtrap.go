8dec70c5c1be1665ba0d8d34cc11d7056551d3d5
needkopi <rizkychandra989@gmail.com>
2019-10-07 23:07:47 +0700

    create default file yaml example

---
+++ cmd/requtrap/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+  "log"
+  "fmt"
+  "os"
+
+  "gopkg.in/yaml.v3"
+)
+
+var(
+  Default = `
+  #server
+  host: "8080"
+
+  #gateway example 1
+  request:
+    url: /test
+    addr: http://127.0.0.1:8000
+  
+
+  #gateway example 2
+  request:
+    url: /coba
+    addr: http://127.0.0.1:8080
+
+
+  `
+)
+
+
+func isError(err error){
+  if err != nil {
+    log.Fatal("error: %v",err)
+  }
+}
+
+func main() {
+  var err error
+  defer isError(err)
+
+  t := yaml.Node{}
+  err = yaml.Unmarshal([]byte(Default),&t)
+  if err != nil {return}
+
+  b,err:= yaml.Marshal(&t) 
+  if err != nil {return}
+
+  f,err := os.Create("default_config_example.yaml")
+  if err != nil {return}
+  fmt.Fprintf(f,string(b))
+  err = f.Close() 
+  if err != nil {return} 
+}
