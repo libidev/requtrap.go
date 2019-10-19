317a4d83fd797547dc587eea2029104eac67eb26
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2019-10-19 19:05:07 +0700

    add helpful help message

--- cli/action/main.go
+++ cli/action/main.go
@@ -7,7 +7,28 @@ import (
 )
 
 func Help() {
-	fmt.Println("help")
+	fmt.Println(" ____                 _____")
+	fmt.Println("|  _ \\ ___  __ _ _   |_   _| __ __ _ _ __")
+	fmt.Println("| |_) / _ \\/ _` | | | || || '__/ _` | '_ \\")
+	fmt.Println("|  _ <  __/ (_| | |_| || || | | (_| | |_) |")
+	fmt.Println("|_| \\_\\___|\\__, |\\__,_||_||_|  \\__,_| .__/")
+	fmt.Println("	      |_|                   |_|")
+	fmt.Println("RequTrap - Fast and Configurable API Gateway")
+
+	fmt.Println(`
+Usage: 
+	requtrap [command]
+
+Commands: 
+	start [config.yaml]	starting API Gateway
+	stop               	stopping API Gateway
+	help               	show this help message
+
+
+Example:
+	requtrap start config.yaml
+	requtrap stop
+`)
 }
 
 func Start(conf *config.ConfigYaml) {
