c312e6ef9102cd68399a1872d5f3020cb22753f1
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2019-10-22 14:14:17 +0700

    remove unused json convertion

--- http/main.go
+++ http/main.go
@@ -6,7 +6,7 @@ import (
   "io/ioutil"
   "time"
   "strconv"
-  "encoding/json"
+  // "encoding/json"
   "net/http"
   "github.com/libidev/requtrap.go/cli/config"
 )
@@ -42,18 +42,8 @@ func (h *HttpHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
               log.Fatal(err)
             }
 
-            var result map[string]interface{}
-            err = json.Unmarshal(contents,&result)
-            if err != nil{
-              log.Fatal(err)
-            }
-            fmt.Printf("\nredirect to : %s\n",service.Upstream)
-            fmt.Println("response :")
-
-            js, err := json.Marshal(result)
-            fmt.Println(string(js))
             w.Header().Set("Content-Type", "application/json")
-            w.Write(js)
+            w.Write([]byte(contents))
           }
         }
       }
