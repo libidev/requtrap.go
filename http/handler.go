c3a73d06cc60152972fdeb2f0f6125f7f5b348a1
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2019-10-22 19:42:30 +0700

    create gateway method for forwarding section

--- http/handler.go
+++ http/handler.go
@@ -2,7 +2,6 @@ package http
 
 import (
 	"bytes"
-	"encoding/json"
 	"fmt"
 	"github.com/libidev/requtrap.go/cli/config"
 	"io/ioutil"
@@ -15,78 +14,67 @@ type HttpHandler struct {
 	Routes []config.ConfigService
 }
 
-func (h HttpHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	//fmt.Println(h.GetRequestMethod(r))
-
-	if r.URL.Path != "favicon.ico" {
-		for _, service := range h.Routes {
-			if r.URL.Path == service.Path {
-				//GATEWAY
-				tr := &http.Transport{
-					MaxIdleConns:       10,
-					IdleConnTimeout:    30 * time.Second,
-					DisableCompression: true,
-				}
-
-				url := service.Upstream + service.Path
-				client := &http.Client{Transport: tr}
-				if h.GetRequestMethod(r) == "GET" {
-					resp, err := client.Get(url)
-					if err != nil {
-						log.Fatal(err)
-					} else {
-						defer resp.Body.Close()
-
-						if resp.StatusCode == http.StatusOK {
-							contents, err := ioutil.ReadAll(resp.Body)
-							if err != nil {
-								log.Fatal(err)
-							}
-
-							var result []map[string]interface{}
-							err = json.Unmarshal(contents, &result)
-							if err != nil {
-								log.Fatal(err)
-							}
-							fmt.Printf("\nredirect to : %s\n", service.Upstream)
-							fmt.Println("response :")
-							js, err := json.Marshal(result)
-							fmt.Println(string(js))
-							w.Header().Set("Content-Type", "application/json")
-							w.Write(js)
-						}
-					}
-				} else if h.GetRequestMethod(r) == "POST" {
-
-					var jsonStr, err = ioutil.ReadAll(r.Body)
-					if err != nil {
-						log.Fatal(err)
-					}
-
-					req, err := http.NewRequest("POST", url, bytes.NewBuffer([]byte(jsonStr)))
-					req.Header.Set("X-Custom-Header", "myvalue")
-					req.Header.Set("Content-type", "application/json")
-
-					resp, err := client.Do(req)
-					if err != nil {
-						log.Fatal(err)
-					}
-					defer resp.Body.Close()
-
-					fmt.Println("response status : ", resp.Status)
-					fmt.Println("response Header : ", resp.Header)
-					body, _ := ioutil.ReadAll(resp.Body)
-					fmt.Println("response body : ", string(body))
-				}
-			}
-		}
-	}
-}
-
 func (h HttpHandler) GetRequestMethod(r *http.Request) string {
 	return r.Method
 }
 
 func (h *HttpHandler) AddRoute(service config.ConfigService) {
 	h.Routes = append(h.Routes, service)
+}
+
+func (h HttpHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "favicon.ico" {
+		for _, service := range h.Routes {
+			path := r.URL.Path
+			if path == service.Path {
+				h.Gateway(w, r)(service)
+			}
+		}
+	}
+}
+
+func (h HttpHandler) Gateway(w http.ResponseWriter, r *http.Request) func(config.ConfigService) {
+	var reqbody []byte
+	var err error
+
+	if h.GetRequestMethod(r) == http.MethodPost {
+		reqbody, err = ioutil.ReadAll(r.Body)
+		defer r.Body.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	return func(service config.ConfigService) {
+		url := service.Upstream + service.Path
+		tr := &http.Transport{
+			MaxIdleConns:       10,
+			IdleConnTimeout:    30 * time.Second,
+			DisableCompression: true,
+		}
+		client := &http.Client{Transport: tr}
+		req, err := http.NewRequest(h.GetRequestMethod(r), url, bytes.NewBuffer(reqbody))
+		req.Header.Set("Content-type", "application/json")
+
+		resp, err := client.Do(req)
+		if err != nil {
+			log.Fatal(err)
+		}
+		body, err := ioutil.ReadAll(resp.Body)
+		defer resp.Body.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println("")
+		fmt.Println("Status :", resp.Status)
+		fmt.Println("Header :")
+		for k, v := range resp.Header {
+			fmt.Println("  ", k + ":", v[0])
+		}
+		fmt.Println("Body   :", string(body))
+
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(body)
+	}
 }
