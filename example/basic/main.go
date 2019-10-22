452019bb68e1d5a36ea1384999c5d4f9abbcfd5b
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2019-10-22 14:01:14 +0700

    add post request for example

--- example/basic/main.go
+++ example/basic/main.go
@@ -4,29 +4,55 @@ import (
   "log"
   "net/http"
   "encoding/json"
+  "io/ioutil"
 )
 
-type Books struct{
-  Name string
-  Pages int
+type Book struct{
+  Name string `json:"name"`
+  Pages int  `json:"pages"`
 }
 
-func getBooks(w http.ResponseWriter,r *http.Request){
-  books := Books{"Hello",18}
+var books []Book
 
-  js, err := json.Marshal(books)
-  if err != nil{
-    http.Error(w,err.Error(),http.StatusInternalServerError)
-    log.Fatal(err)
-    return
+func getBooks(w http.ResponseWriter, r *http.Request){
+  if r.Method == http.MethodGet {
+    // GET
+    js, err := json.Marshal(books)
+    if err != nil{
+      http.Error(w, err.Error(), http.StatusInternalServerError)
+      log.Fatal(err)
+      return
+    }
+  
+    w.Header().Set("Content-Type", "application/json")
+    w.Write(js)
+  } else if r.Method == http.MethodPost {
+    // POST
+    body, err := ioutil.ReadAll(r.Body)
+    defer r.Body.Close()
+    if err != nil{
+      http.Error(w, err.Error(), http.StatusInternalServerError)
+      log.Fatal(err)
+      return
+    }
+    var book Book
+    err = json.Unmarshal(body, &book)
+    if err != nil{
+      http.Error(w, err.Error(), http.StatusInternalServerError)
+      log.Fatal(err)
+      return
+    }
+
+    books = append(books, book)
+  
+    w.Header().Set("Content-Type", "application/json")
+    w.Write([]byte("OK"))
   }
-
-  w.Header().Set("Content-Type","application/json")
-  w.Write(js)
 }
 
 
 func main(){
-  http.HandleFunc("/books",getBooks)
-  http.ListenAndServe(":8001",nil)
+  books = append(books, Book{"Hello", 18})
+  http.HandleFunc("/books", getBooks)
+  http.ListenAndServe(":8001", nil)
 }
